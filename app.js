8a81feaefc8850951d5bb80c4e065aac1678f523
Abe Ribeiro <abe@tundra.com.au>
2016-02-25 11:02:02 +1100

    added commandline param to skip videos

--- app.js
+++ app.js
@@ -9,22 +9,28 @@ var readFileAsBuffer = require("./scripts/utils/readFileAsBuffer.js"),
 
 
 
+var datafile = "data.json";
+var configfile = "config.json";
+var buildVideosFlag = true;
+
+var args = require('minimist')(process.argv.slice(2));
+
+if(args['datafile']){
+	datafile = args['datafile'];
+}
+
+if(args['configfile']){
+	configfile = args['configfile'];
+}
+
+if(args['skipvideos']){
+	buildVideosFlag = false
+}
+
+
 // This loads the data and config files, allowing commandline args to specify path to these
 // then turns them into json and combines them into a single object for processing
 function loadFiles(){
-	var datafile = "data.json";
-	var configfile = "config.json";
-
-	var args = require('minimist')(process.argv.slice(2));
-
-	if(args['datafile']){
-		datafile = args['datafile'];
-	}
-
-	if(args['configfile']){
-		configfile = args['configfile'];
-	}
-
 	var dataJSON;
 
 	return readFileAsBuffer(datafile)
@@ -47,10 +53,14 @@ loadFiles()
 .then(buildImages)
 .then(zipOutput)
 .then(generateLinks)
-.then(buildVideos)
+.then(function(result){
+	if(buildVideosFlag){
+		return buildVideos(result);
+	}	
+})
 .catch(handleError);
 
 
 function handleError(error){
-    console.log(error);
+    console.log(error.message);
 }
